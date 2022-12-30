b2b5ba45f141edbc68c316ef968e24bfbfe18cf9
kavyadiju <70419997+kavyadiju@users.noreply.github.com>
2022-12-30 23:10:11 +0530

    Update Code.js

--- js/Code.js
+++ js/Code.js
@@ -8,4 +8,6 @@ function clues() {
     fill("lightblue")
     text("We know you were born on 28 jan 1990.. Task no 2 is to fill in the day in which you were born..", 3100,150)
     text("Here is a clue! Solve the jumpled letters to get the answer and fill it in the box below and press the check button!", 3100,170)
+    text("Note: Please use only CAPITAL letters or small letters while enetering the day", 3100,190)
+
 }
